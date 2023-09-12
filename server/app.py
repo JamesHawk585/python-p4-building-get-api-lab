2d239c30fbdb8cd5e47f6408ebf07d3acf377d27
James Hawk <jph94880@gmail.com>
2023-09-11 22:12:22 -0500

    Adds functionality to bakery_by_id() in app.py

--- server/app.py
+++ server/app.py
@@ -37,10 +37,24 @@ def bakeries():
     )
 
     return response 
+
 # returns a single bakery as JSON with its baked goods nested in an array. Use the id from the URL to look up the correct bakery.
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = {
+            "id": bakery.id,
+            "name": bakery.name,
+            "created_at": bakery.created_at,
+            "updated_at": bakery.updated_at
+    }
+    response = make_response(
+        bakery_dict,
+        200
+    )
+
+    return response
+    
 
 # returns an array of baked goods as JSON, sorted by price in descending order. (HINT: how can you use SQLAlchemy to sort the baked goods in a particular order?)
 @app.route('/baked_goods/by_price')
